2ed9ff5171bc462459e22fd92f9f0e58ea11d836
Haisa Reyes <130813009+HaisaeReyes@users.noreply.github.com>
2023-10-19 22:12:16 +0000

    correcciones 2

--- src/js/app.js
+++ src/js/app.js
@@ -34,7 +34,7 @@ function render(variables = {}) {
             ${cover}
           <img src=" ${variables.avatarURL} " class="photo" />
           <h1> ${variables.name == null ? "Name" : variables.name} 
-            ${variables.lastName == null ? "Last Name" : variables.name} </h1>
+            ${variables.lastName == null ? "Last Name" : variables.lastName} </h1>
           <h2> ${variables.role == null ? "Role" : variables.role} </h2>
           <h3> ${variables.city == null ? "City" : variables.city} 
 		  	${variables.country == null ? "Country" : variables.country} </h3>
@@ -45,7 +45,7 @@ function render(variables = {}) {
             <li><a href="https://github.com/${
               variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${
+            <li><a href="https://linkedin.com/in/${
               variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${
